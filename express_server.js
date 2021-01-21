5323485f391550f1ef6f2a99ceca97fef1c853b4
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-21 00:20:43 +0000

    completed refactoring of login! All user functions are working at this stage for loging in and registering, eslinting done

--- express_server.js
+++ express_server.js
@@ -4,27 +4,36 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
+//---------------------------------------------------Middleware---------------------------------------------------------------------------
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+//---------------------------------------------------Objects for reference/future database----------------------------------------------------------------
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
-}
+};
+
+//---------------------------------------------------HELPER FUNCTIONS---------------------------------------------------------------------------
 
 function getUserByEmail(email) {
   for (let user in users) {
@@ -34,24 +43,35 @@ function getUserByEmail(email) {
       return false;
     }
   }
-};
+}
+
+function getUserByPassword(password) {
+  for (let user in users) {
+    if (users[user].password === password) {
+      return users[user];
+    } else {
+      return false;
+    }
+  }
+}
 
 
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x100000).toString(16);
-};
+}
+//---------------------------------------------------Post Handlers ---------------------------------------------------------------------------
 
 //handler for new users to register, adding to our users object
 app.post("/register", (req, res) => {
   if (req.body.email === "" || req.body.password === "") {
-    console.log(users)
+    console.log(users);
     res.sendStatus(400);
-    console.log(users)
+    console.log(users);
   } else if (getUserByEmail(req.body.email)) {
-    console.log(users)
+    console.log(users);
     res.sendStatus(400);
-    console.log(users)
-  };
+    console.log(users);
+  }
   const short = generateRandomString();
   users[short] = {
     id: short,
@@ -60,7 +80,7 @@ app.post("/register", (req, res) => {
   };
   res.cookie('user_id', short);
   console.log(urlDatabase[short]);
-  res.redirect("/urls")
+  res.redirect("/urls");
 });
 
 
@@ -74,15 +94,26 @@ app.post("/urls", (req, res) => {
 
 //add post function to handle users logging in
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username); //change to user_id, but also need to accept username/password?
-  res.redirect("/urls");
+  if (!getUserByEmail(req.body.email)) {
+    //console.log(users)
+    res.sendStatus(403);
+    //console.log(users)
+  } else if (!getUserByPassword(req.body.password)) {
+    //console.log(users)
+    res.sendStatus(403);
+    //console.log(users)
+  } else {
+    res.cookie('user_id', getUserByEmail(req.body.email).id);
+    res.redirect("/urls");
+  }
 });
+
 
 //add post function to handle users logging out
 app.post("/logout", (req, res) => {
-  console.log(req.cookies)
-  console.log(req.cookies["username"]);
-  res.clearCookie('username', req.cookies["username"]);//change to user_id, but also need to accept username/password?
+  //console.log(req.cookies)
+  //console.log(req.cookies["user_id"]);
+  res.clearCookie('user_id', req.cookies["user_id"]);
   res.redirect("/urls");
 });
 
@@ -90,24 +121,48 @@ app.post("/logout", (req, res) => {
 //   urlDatabase[req.params.shortURL] = req.body.longURL;
 //   res.redirect("/urls");
 // });
-
+  
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
-
+  
+//---------------------------------------------------Get handlers---------------------------------------------------------------------------
+  
+  
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+  
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+  
+  
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+  
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+//---------------------------------------------------Page renderers---------------------------------------------------------------------------
+  
+//render login page when user attempts to login
+app.get("/login", (req, res) => {
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
+  res.render("urls_login", templateVars);
+});
 
-app.get("/urls", (req, res) => { 
-  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]]};// previous code that was here for reference username: req.cookies["username"]};
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };// previous code that was here for reference username: req.cookies["username"]};
   res.render("urls_index", templateVars); //doubting users is correct, should be the object for the user...user_id: users[req.cookies["username"]]
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    user_id: users[req.cookies["username"]]
+    urls: urlDatabase, user: users[req.cookies["user_id"]]//user_id: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
@@ -124,20 +179,4 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
 
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
